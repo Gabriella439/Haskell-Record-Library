44503c84bd10b43c00a988355bbf9e7328e2d7d1
Gabriel Gonzalez <ggonzalez@twitter.com>
2014-06-30 16:23:12 -0700

    Removed line buffering from `stdout` and `stderr`
    This is so that prompts don't get swallowed

--- src/Record.hs
+++ src/Record.hs
@@ -13,51 +13,53 @@ import MVC.Prelude (producer, stdoutLines)
 import qualified Pipes.Prelude as Pipes
 import System.Environment (getArgs)
 import System.Process
-import System.IO (Handle, IOMode(AppendMode), hPrint, hPutStr, hPutStrLn)
+import qualified System.IO as IO
 import Prelude hiding (id, FilePath)
 
-run :: String -> String -> [String] -> ReaderT Handle IO ()
+run :: String -> String -> [String] -> ReaderT IO.Handle IO ()
 run label cmd args = ReaderT $ \handle -> do
-    hPutStr handle (label ++ ": ")
+    IO.hPutStr handle (label ++ ": ")
     (i, e) <- fmap (\(_, x, y) -> (x, y)) (readProcessWithExitCode cmd args "")
         `catch` (\e -> do
             let _ = e :: SomeException
             return ("", show e) )
-    hPutStr handle (if null e then format i else "N/A\n")
+    IO.hPutStr handle (if null e then format i else "N/A\n")
 
-branch :: ReaderT Handle IO ()
+branch :: ReaderT IO.Handle IO ()
 branch = run "Branch " "git" ["rev-parse", "--abbrev-ref", "HEAD"]
 
-sha :: ReaderT Handle IO ()
+sha :: ReaderT IO.Handle IO ()
 sha = run "SHA    " "git" ["rev-parse", "--verify", "HEAD"]
 
-diff :: ReaderT Handle IO ()
+diff :: ReaderT IO.Handle IO ()
 diff = run "Diff   " "git" ["diff"]
 
-hostname :: ReaderT Handle IO ()
+hostname :: ReaderT IO.Handle IO ()
 hostname = run "Host   " "hostname" []
 
-pwd :: ReaderT Handle IO ()
+pwd :: ReaderT IO.Handle IO ()
 pwd = run "PWD    " "pwd" []
 
-time :: ReaderT Handle IO ()
+time :: ReaderT IO.Handle IO ()
 time = run "Date   " "date" []
 
-user :: ReaderT Handle IO ()
+user :: ReaderT IO.Handle IO ()
 user = run "User   " "whoami" []
 
 format :: String -> String
 format = unlines . transform . lines
   where
     transform (l:ls) = l:map ("         " ++) ls
-    transform  x     = x
+    transform  []    = [""]
 
 -- The command to run
 main :: IO ()
 main = do
+    IO.hSetBuffering IO.stdout IO.NoBuffering
+    IO.hSetBuffering IO.stderr IO.NoBuffering
     args <- getArgs
     home <- getHomeDirectory
-    withFile (home </> ".record") AppendMode $ \handle -> (do
+    withFile (home </> ".record") IO.AppendMode $ \handle -> (do
         flip runReaderT handle $ do
             time
             user
@@ -66,7 +68,7 @@ main = do
             branch
             sha
             diff
-        hPutStrLn handle ("Command: " ++ unwords args)
+        IO.hPutStrLn handle ("Command: " ++ unwords args)
         case args of
             []       -> return ()
             cmd:rest -> do
@@ -76,14 +78,16 @@ main = do
                     Nothing           -> return ()
                     Just (hOut, hErr) -> do
                         runMVC () id $ do
-                            c <- fmap (fmap ("O: " ++)) (producer Single (Pipes.fromHandle hOut))
-                              <> fmap (fmap ("E: " ++)) (producer Single (Pipes.fromHandle hErr))
-                            let v = asSink (hPutStrLn handle)
+                            let stdOut = producer Single (Pipes.fromHandle hOut)
+                                stdErr = producer Single (Pipes.fromHandle hErr)
+                            c <- fmap (fmap ("O: " ++)) stdOut
+                              <> fmap (fmap ("E: " ++)) stdErr
+                            let v = asSink (IO.hPutStrLn handle)
                                  <> stdoutLines
                             return (v, c)
 
-        hPutStrLn handle "" )
+        IO.hPutStrLn handle "" )
         `catch` (\e -> do
-            hPrint handle (e :: SomeException)
-            hPutStrLn handle ""
+            IO.hPrint handle (e :: SomeException)
+            IO.hPutStrLn handle ""
             throwIO e )
