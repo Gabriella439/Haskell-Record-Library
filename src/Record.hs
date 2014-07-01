4ea543b7fdf426641a050f7b413359f3fd119c19
Gabriel Gonzalez <ggonzalez@twitter.com>
2014-06-30 20:09:00 -0700

    Simplified code further

--- src/Record.hs
+++ src/Record.hs
@@ -5,7 +5,6 @@ module Main where
 import Control.Applicative ((<$>), (<*>))
 import Control.Concurrent.Async (mapConcurrently)
 import Control.Exception (SomeException, catch, throwIO)
-import Control.Monad.Trans.Reader (ReaderT(ReaderT), runReaderT)
 import qualified Data.ByteString as ByteString
 import Data.Foldable (forM_)
 import Filesystem (getHomeDirectory, withFile)
@@ -16,70 +15,49 @@ import System.Environment (getArgs)
 import System.Process
 import qualified System.IO as IO
 
-run :: String -> String -> [String] -> ReaderT IO.Handle IO ()
-run label cmd args = ReaderT $ \handle -> do
-    IO.hPutStr handle (label ++ ": ")
-    (i, e) <- fmap (\(_, x, y) -> (x, y)) (readProcessWithExitCode cmd args "")
-        `catch` (\e -> do
-            let _ = e :: SomeException
-            return ("", show e) )
-    IO.hPutStr handle (if null e then format i else "N/A\n")
-
-branch :: ReaderT IO.Handle IO ()
-branch = run "Branch " "git" ["rev-parse", "--abbrev-ref", "HEAD"]
-
-sha :: ReaderT IO.Handle IO ()
-sha = run "SHA    " "git" ["rev-parse", "--verify", "HEAD"]
-
-diff :: ReaderT IO.Handle IO ()
-diff = run "Diff   " "git" ["diff"]
-
-hostname :: ReaderT IO.Handle IO ()
-hostname = run "Host   " "hostname" []
-
-pwd :: ReaderT IO.Handle IO ()
-pwd = run "PWD    " "pwd" []
-
-time :: ReaderT IO.Handle IO ()
-time = run "Date   " "date" []
-
-user :: ReaderT IO.Handle IO ()
-user = run "User   " "whoami" []
-
 format :: String -> String
 format = unlines . transform . lines
   where
     transform (l:ls) = l:map ("         " ++) ls
     transform  []    = [""]
 
--- The command to run
 main :: IO ()
 main = do
-    IO.hSetBuffering IO.stdout IO.NoBuffering
-    IO.hSetBuffering IO.stderr IO.NoBuffering
     args <- getArgs
     home <- getHomeDirectory
     withFile (home </> ".record") IO.AppendMode $ \handle -> (do
-        flip runReaderT handle $ do
-            time
-            user
-            hostname
-            pwd
-            branch
-            sha
-            diff
+        let run label cmd args' = do
+                let trim (_, x, y) = (x, y)
+
+                IO.hPutStr handle (label ++ ": ")
+                (i, e) <- fmap trim (readProcessWithExitCode cmd args' "")
+                    `catch` (\e -> do
+                        let _ = e :: SomeException
+                        return ("", show e) )
+                IO.hPutStr handle (if null e then format i else "N/A\n")
+
+        run "Date   " "date" []
+        run "User   " "whoami" []
+        run "Host   " "hostname" []
+        run "PWD    " "pwd" []
+        run "Branch " "git" ["rev-parse", "--abbrev-ref", "HEAD"]
+        run "SHA    " "git" ["rev-parse", "--verify", "HEAD"]
+        run "Diff   " "git" ["diff"]
+
         IO.hPutStrLn handle ("Command: " ++ unwords args)
         case args of
             []       -> putStrLn "Usage: record COMMAND [ARGUMENT ...]"
             cmd:rest -> do
                 (_, mOut , mErr, _) <- createProcess (proc cmd rest)
                     { std_out = CreatePipe, std_err = CreatePipe }
-                forM_ ((,) <$> mOut <*> mErr) $ \(hOut, hErr) ->
-                    flip mapConcurrently [hOut, hErr] $ \hSrc -> do
-                        IO.hSetBuffering hSrc IO.NoBuffering
+                forM_ ((,) <$> mOut <*> mErr) $ \(hOut, hErr) -> do
+                    let handlePairs = [(hOut, IO.stdout), (hErr, IO.stderr)]
+                    flip mapConcurrently handlePairs $ \(hSrc, hDest) -> do
+                        IO.hSetBuffering hSrc  IO.NoBuffering
+                        IO.hSetBuffering hDest IO.NoBuffering
                         runEffect $ for (fromHandle hSrc) $ \bs -> lift $ do
+                            ByteString.hPutStr hDest  bs
                             ByteString.hPutStr handle bs
-                            ByteString.putStr         bs
         IO.hPutStrLn handle "" )
         `catch` (\e -> do
             IO.hPrint handle (e :: SomeException)
