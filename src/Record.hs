ee153114afb6b6a74019c1364f207e84facba5cb
Gabriel Gonzalez <ggonzalez@twitter.com>
2014-09-27 22:52:45 -0700

    Use `text`

--- src/Record.hs
+++ src/Record.hs
@@ -5,6 +5,8 @@ module Main where
 import Control.Concurrent.Async (race_)
 import Control.Exception (SomeException, catch, finally, throwIO)
 import Control.Monad (forM_)
+import qualified Data.Text.Lazy    as Text
+import qualified Data.Text.Lazy.IO as Text
 import Filesystem (getHomeDirectory, withFile)
 import Filesystem.Path ((</>))
 import System.Environment (getArgs)
@@ -49,10 +51,10 @@ main = do
                 let redirect src dest = do
                         hSetBuffering src  NoBuffering
                         hSetBuffering dest NoBuffering
-                        bs <- hGetContents src
-                        forM_ (lines bs) $ \line -> do
-                            hPutStrLn dest   line
-                            hPutStrLn handle line
+                        bs <- Text.hGetContents src
+                        forM_ (Text.lines bs) $ \line -> do
+                            Text.hPutStrLn dest   line
+                            Text.hPutStrLn handle line
 
                 redirect out stdout `race_` redirect err stderr )
         `finally` (hPutStrLn handle "") )
